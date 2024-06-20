63abe127aaf2328d54b6d5e3285847eafd115035
Central <central@Javiers-MacBook-Air.local>
2024-06-20 10:19:14 -0600

    Descripción de los cambios

---
+++ main.js
@@ -0,0 +1,15 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Código para ajustar el ancho de los bloques de código, si es necesario.
+    const codeBlocks = document.querySelectorAll('pre code');
+
+    codeBlocks.forEach(block => {
+        const parentPre = block.parentElement;
+        const parentContainer = parentPre.parentElement;
+
+        block.style.maxWidth = parentContainer.offsetWidth + 'px';
+        parentPre.style.maxWidth = parentContainer.offsetWidth + 'px';
+        parentPre.style.overflowX = 'hidden';
+    });
+});
